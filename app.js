205977e2d2f56ddfffcf9763be33ab93ae742a57
Ahmed Raza <ahmedr.0331@gmail.com>
2024-11-06 16:39:44 +0500

    done read less and read more functionality

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const expandText = (ele) =>{
     if(isExpend == false){
      ele.previousSibling.style.display = 'block'
      isExpend = true
+     ele.innerHTML = 'read more'
+     
     }else{
         ele.previousSibling.style.display = 'none'
         isExpend = false
+        ele.innerHTML = 'read less'
     }
 }
